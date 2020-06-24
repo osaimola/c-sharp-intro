c3f81c76f435a7c9bce2070854c908735d45e1ed
Osa <osaimola@gmail.com>
2020-06-24 15:14:34 -0600

    Add an assignment and output concatenated hello string.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,10 @@ namespace c_sharp_intro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, TECHCareers!");
+            // assign "osa" to the myName variable
+            string myName = "Osa";
+            // use the + operator to concatenate Hello and the value of myName variable
+            Console.WriteLine("Hello, " + myName);
         }
     }
 }
