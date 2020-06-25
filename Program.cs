9883ae688194e117103fe5008b3700c30e36118b
Osa <osaimola@gmail>
2020-06-25 10:32:05 -0600

    added datatype and math operators

--- Program.cs
+++ Program.cs
@@ -6,18 +6,41 @@ namespace c_sharp_intro
     {
         static void Main(string[] args)
         {
-            // assign "osa" to the myName variable
-            string myName = "Osa";
-            // use the + operator to concatenate Hello and the value of myName variable
-            Console.WriteLine("Hello, " + myName);
+            // "=" is the assignment operator
+            // String datatype is used for text
+            string myString = "this is a string";
 
-            Console.WriteLine("Enter your name followed by the Enter key...");
-            // assign string entered from the console to the username Variable
-            string userName = Console.ReadLine();
-            // print entered string to console using string interpolation.
-            Console.WriteLine($"Thanks {userName}");
-            // Substring takes two arguements. !. starting position 2. Number of characters to return
-            Console.WriteLine("1234567890".Substring(2, 5));
+            // Integer datatype is used for whole numbers (no decimal)
+            int myInt = 42;
+
+            // Double datatype is used for decimal numbers
+            double myDouble = 69.99;
+
+            // Boolean datatype is used for true/false values
+            bool myBool = true;
+
+            // Subtraction operator is "-"
+            Console.WriteLine(myInt - 10); //32
+
+            // Multiplication operator is "*"
+            Console.WriteLine(myInt * 100); //4200
+
+            // Division operator is "/"
+            Console.WriteLine(myInt / 2); //21
+
+            // Modulus operator is "%" (it returns the remainder after division)
+            Console.WriteLine(myInt % 5); //2 (8, remainder 2)
+
+            // Addition operator is "+"
+            // But WAIT!!! This is also the concatenation operator
+            Console.WriteLine(myInt + 8); //50
+
+            Console.WriteLine("String " + 124); // Mixing string with int makes concatenation => String124
+            Console.WriteLine("String" + 5 + 5); // If string comes first everything is concatenated => String55
+            Console.WriteLine(5 + 5 + "String"); // calculations before strings are handled before concatenation => 10String
+
+
+
         }
     }
 }
