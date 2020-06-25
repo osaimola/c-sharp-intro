2ead7d34c658168d807ccff536bd47d7f66ed1b6
Osa <osaimola@gmail>
2020-06-25 13:18:50 -0600

    add conditional if statment

--- Program.cs
+++ Program.cs
@@ -6,52 +6,20 @@ namespace c_sharp_intro
     {
         static void Main(string[] args)
         {
-            // Logical operators
-            // The OR "||" will evaluate to TRUE if EITHER side is TRUE
-            Console.WriteLine(true || true); // true
-            Console.WriteLine(false || true); // true
-            Console.WriteLine(false || false); // false
+            // Prompt the user to enter a text to be stored in the userInput
+            Console.WriteLine("Put in some text if you dare...");
+            string userInput = Console.ReadLine();
 
-            // The AND "&&" will evaluate to TRUE only if BOTH sides are TRUE
-            Console.WriteLine(true && true); // true
-            Console.WriteLine(false && true); // false
-            Console.WriteLine(false && false); // false
-
-            // How does this behave with other data types?
-            // Console.WriteLine(true && 1); // IT DOESN"T WORK
-
-            // Comparative operators
-            // "Equal" to check
-            Console.WriteLine(false == false); // true
-            Console.WriteLine(true == false); // false
-            Console.WriteLine(2.5 == 2.5); // true
-            Console.WriteLine(2.25 == 3.15); // false
-            // Console.WriteLine("2.25" == 2.25); // cannot compare two different types
-
-            // Greater than checks
-            Console.WriteLine(6 > 7); // false
-            Console.WriteLine(10 > 5); // True
-
-            // Less than checks
-            Console.WriteLine(6 < 7); // false
-            Console.WriteLine(10 < 5); // false
-
-            // Greater than or Equal to checks
-            Console.WriteLine(5 >= 5); // True
-            Console.WriteLine(10 >= 5); // True
-            Console.WriteLine(4 >= 5); // False
-
-            // Less than or Equal to checks
-            Console.WriteLine(5 <= 5); // True
-            Console.WriteLine(10 <= 5); // False
-            Console.WriteLine(4 <= 5); // True
-
-            // Not Equal to check
-            Console.WriteLine(false != false); // False
-            Console.WriteLine(false != true); // Trues
-            Console.WriteLine(5 != 5); // false
-            Console.WriteLine(5 != 10); // true
-
+            // the if statement will run if the  IF CONTDITION is TRUE or has a truthy value
+            if (userInput.Length > 0)
+            {
+                Console.WriteLine("the user entered a string");
+            }
+            // the else statement will run
+            else
+            {
+                Console.WriteLine("hmmm... looks like the user did not enter any values");
+            }
         }
     }
 }
